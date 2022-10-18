d763c07485c5c54739733d2abe156a1018e5aa47
Tinghan Chen(Allen) <chentinghan@outlook.com>
2022-10-18 17:43:24 +1100

    chore: construct the project basic folder structure

---
+++ src/pages/university-list/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListItem from "./List";
+
+const UniversityList = () => {
+  return (
+    <div>
+      <ListItem />
+    </div>
+  );
+};
+
+export default UniversityList;
