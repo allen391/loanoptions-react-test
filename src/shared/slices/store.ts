44e664c4a934d9159d69c2c4103719271ae34d80
Tinghan Chen(Allen) <chentinghan@outlook.com>
2022-10-18 19:58:35 +1100

    feat: construct the redux toolbox structure

---
+++ src/shared/slices/store.ts
@@ -0,0 +1,51 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { IUniItem } from '../types';
+import { Config, http } from '../utils';
+
+interface IUniState {
+  error?: string | null;
+  data?: IUniItem[];
+  loading: boolean;
+}
+
+const initialState: IUniState = {
+  error: null,
+  data: [],
+  loading: false,
+};
+
+export const getUniList = createAsyncThunk(
+  'university/getUniList',
+  async ({ endpoint, config }: { endpoint: string; config: Config }) => {
+    return await http(endpoint, config);
+  },
+);
+export const uniSlice = createSlice({
+  name: 'university',
+  initialState,
+  reducers: {
+    addItem: (state) => {
+      console.log(state);
+    },
+    deleteItem: (state) => {
+      console.log(state);
+    },
+  },
+  // for api call async operations
+  extraReducers: {
+    [getUniList.pending.type]: (state) => {
+      state.loading = true;
+    },
+    [getUniList.fulfilled.type]: (state, { payload }) => {
+      state.loading = false;
+      state.data = payload;
+    },
+    [getUniList.rejected.type]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
+});
+
+export const { addItem, deleteItem } = uniSlice.actions;
+export default uniSlice.reducer;
